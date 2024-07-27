64b477b40d2999c3b80b06bfc3c94ac7535e4f5d
harisnaseer24 <harisnaseer258@gmail.com>
2024-07-27 14:22:31 +0500

    CRUD OPERATTIONS COMPLETED with file uploading

--- TempEmbeddin2302C2/TempEmbeddin2302C2/Controllers/ItemController.cs
+++ TempEmbeddin2302C2/TempEmbeddin2302C2/Controllers/ItemController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.Elfie.Model.Map;
 using Microsoft.EntityFrameworkCore;
 
 using TempEmbeddin2302C2.Models;
@@ -53,7 +54,74 @@ namespace TempEmbeddin2302C2.Controllers
             return RedirectToAction("Index");
         }
 
+        public IActionResult Edit(int id)
+        {
+            var item = db.Items.Find(id);
+            if(item != null)
+            {
+                ViewBag.CatId = new SelectList(db.Categories, "CatId", "CatName");
+                return View(item);
+            }
+            else
+            {
+                ViewBag.errorMsg = "Product not available.";
+                return RedirectToAction();
+            }
 
+        }
+        [HttpPost]
+        public IActionResult Edit(Item item, IFormFile file, string oldImage)
+        {
+            if (file != null && file.Length > 0)
+            {
+                string imagename = DateTime.Now.ToString("yymmddhhmmss");//2410152541245412
+                imagename += "-" + Path.GetFileName(file.FileName);//2410152541245412-sonata.jpg
+
+                var imagepath = Path.Combine(HttpContext.Request.PathBase.Value, "wwwroot/Uploads");
+                var imageValue = Path.Combine(imagepath, imagename);
+
+                using (var stream = new FileStream(imageValue, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+                var dbimage = Path.Combine("/Uploads", imagename);//Uploads/2410152541245412-sonata.jpg
+                item.Image = dbimage;
+                db.Items.Update(item);
+                db.SaveChanges();
+            }
+            else
+            {
+                item.Image = oldImage;
+                db.Items.Update(item);
+                db.SaveChanges();
+            }
+
+            ViewBag.CatId = new SelectList(db.Categories, "CatId", "CatName");
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var item = db.Items.Find(id);
+            if (item != null)
+            {
+                return View(item);
+            }
+            else
+            {
+                return RedirectToAction();
+            }
+        }
+            [HttpPost]
+
+            public IActionResult Delete(Item item)
+            {
+
+            db.Items.Remove(item);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+            }
 
     }
 }
